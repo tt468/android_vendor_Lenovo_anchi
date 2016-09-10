602a997401862cbc13ce5139b8a81ef6eb3f979d
thp1997 <tirthpatel49@gmail.com>
2016-09-10 10:55:01 +0530

    Initial Commit

---
+++ A311-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/proprietary/vendor,system/vendor)
